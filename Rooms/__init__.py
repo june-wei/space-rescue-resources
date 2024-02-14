4f6a11686ca59a1cb0800561bc03b25b82e74a3a
june-wei <158542845+june-wei@users.noreply.github.com>
2024-02-14 13:43:54 +1000

    Created Spaceship

--- Rooms/__init__.py
+++ Rooms/__init__.py
@@ -1,12 +1,2 @@
-from GameFrame import Level
-from Objects.Ship import Ship
-
-class GamePlay(Level):
-    def __init__(self, screen, joysticks):
-        Level.__init__(self, screen, joysticks)
-        
-        # set background image
-        self.set_background_image("Background.png")
-        
-        # add objects
-        self.add_room_object(Ship(self, 25, 50))
+from Rooms.WelcomeScreen import WelcomeScreen
+from Rooms.GamePlay import GamePlay
